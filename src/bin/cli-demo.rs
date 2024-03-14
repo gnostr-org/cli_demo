ebae9fbec4d6a4adabfbef66b82bea3201dce74b
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-13 23:17:12 -0400

    intermediate

--- src/bin/cli-demo.rs
+++ src/bin/cli-demo.rs
@@ -1,15 +1,57 @@
-use git2::*;
 use std::env;
 use std::path::PathBuf;
+use std::process;
 use std::process::Command;
+use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
+use chrono::prelude::*;
 
+
+use cli_demo;
+use cli_demo::options;
+
+#[allow(dead_code)]
 const BITCOIN_GENESIS: &str = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f";
 
+fn get_epoch_ms() -> u128 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis()
+}
+
+#[allow(dead_code)]
 fn get_current_working_dir() -> std::io::Result<PathBuf> {
     env::current_dir()
 }
 
+//repo status CLEAN not enough
+fn ensure_no_unstaged_changes(repo: &mut git2::Repository) -> Result<(), &'static str> {
+    let mut opts = git2::StatusOptions::new();
+    let mut m = git2::Status::empty();
+    let statuses = repo.statuses(Some(&mut opts)).unwrap();
+
+    m.insert(git2::Status::WT_NEW);
+    m.insert(git2::Status::WT_MODIFIED);
+    m.insert(git2::Status::WT_DELETED);
+    m.insert(git2::Status::WT_RENAMED);
+    m.insert(git2::Status::WT_TYPECHANGE);
+
+    for i in 0..statuses.len() {
+        let status_entry = statuses.get(i).unwrap();
+        if status_entry.status().intersects(m) {
+            println!("Please stash all unstaged changes before running.");
+            //return Err("Please stash all unstaged changes before running.");
+            process::exit(1)
+        }
+    }
+
+    Ok(())
+}
+
 fn git_init() -> core::result::Result<(), git2::Error> {
+    let now = SystemTime::now();
+    println!("{:?}",now);
+
     #[allow(clippy::if_same_then_else)]
     let mut command = Command::new("printenv");
 
@@ -48,9 +90,28 @@ fn git_init() -> core::result::Result<(), git2::Error> {
         .unwrap();
     println!("git_init:{}", _result);
 
+
+
+   let start = time::get_time();
+    let epoch = get_epoch_ms();
+    println!("{}", epoch);
+    let system_time = SystemTime::now();
+
+    let datetime: DateTime<Utc> = system_time.into();
+    println!("{}", datetime.format("%d/%m/%Y %T/%s"));
+    println!("{}", datetime.format("%d/%m/%Y %T"));
+
+
+
     let mut git_empty_commit = Command::new("git");
     git_empty_commit
-        .args(["commit", "--allow-empty", "-m", "init", "--no-gpg-sign"])
+        .args([
+            "commit",
+            "--allow-empty",
+            "-m",
+            &epoch.to_string(),
+            "--no-gpg-sign",
+        ])
         .output()
         .expect("failed to execute process");
 
@@ -60,67 +121,44 @@ fn git_init() -> core::result::Result<(), git2::Error> {
         .unwrap();
     println!("git_init:{}", _result);
 
-    // let initialize = if cfg!(target_os = "windows") {
-    //     command
-    //         .args(["/C", "commit --allow-empty -m init commit", ""])
-    //         .output()
-    //         .expect("failed to execute process")
-    // //} else if cfg!(target_os = "macos") {
-    // //    command
-    // //        .args([
-    // //            "commit --allow-empty -m init commit",
-    // //            "",
-    // //        ])
-    // //        .output()
-    // //        .expect("failed to execute process")
-    // //} else if cfg!(target_os = "linux") {
-    // //    command
-    // //        .args([
-    // //            "commit --allow-empty -m init commit",
-    // //            "",
-    // //        ])
-    // //        .output()
-    // //        .expect("failed to execute process")
-    // } else {
-    //     let mut command = Command::new("sh");
-    //     command
-    //         .args(["git commit --allow-empty -m init commit", ""])
-    //         //.arg("git -h &&")
-    //         //  //.args([
-    //         //  //    "git",
-    //         //  //    "commit",
-    //         //  //    "--allow-empty",
-    //         //  //    "-m",
-    //         //  //    "init commit",
-    //         //  //    "",
-    //         //  //])
-    //         .output()
-    //         .expect("failed to execute process")
-    // };
-
-    // let result = String::from_utf8(initialize.stdout)
-    //     .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
-    //     .unwrap();
-    // println!("___________{}", result);
-
-    //let repo_path = ".";
-    //let _repo = Repository::init(repo_path)?;
-    //let _repo = match Repository::open(".") {
-    //    Ok(_repo) => _repo,
-    //    Err(e) => panic!("Error opening repository: {}", e),
-    //};
     Ok(())
 }
 fn main() -> core::result::Result<(), git2::Error> {
-    let _init_result = git_init();
-    //println!("_init_result={:?}", _init_result);
+    let get_pwd = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/C", "echo %cd%"])
+            .output()
+            .expect("failed to execute process")
+    } else if cfg!(target_os = "macos") {
+        Command::new("sh")
+            .arg("-c")
+            .arg("echo ${PWD##*/}")
+            .output()
+            .expect("failed to execute process")
+    } else if cfg!(target_os = "linux") {
+        Command::new("sh")
+            .arg("-c")
+            .arg("echo ${PWD##*/}")
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg("echo ${PWD##*/}")
+            .output()
+            .expect("failed to execute process")
+    };
 
-    //let repo_path = ".";
-    //let repo = Repository::init(repo_path)?;
-    //let repo = match Repository::open(".") {
-    //    Ok(repo) => repo,
-    //    Err(e) => panic!("Error opening repository: {}", e),
-    //};
-    //let status = repo.statuses()?;
+    let pwd = String::from_utf8(get_pwd.stdout)
+        .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
+        .unwrap();
+    //println!("pwd={}", pwd);
+
+    let mut opts = options::Options {
+        message: pwd,
+        repo: ".".to_string(),
+    };
+
+    let _init_result = git_init();
     Ok(())
 }
